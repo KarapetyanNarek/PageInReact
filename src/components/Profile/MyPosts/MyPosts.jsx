9f0b2666d07db0483fd696798ce80dbe00f4a2cb
Narek Karapetyan <narek.karapetyan.v@gmail.com>
2019-08-19 14:30:30 +0400

    page-version 1.25 (UI-BLL)

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
+
+    let postData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 11}
+    ];
+
+
     return (
             <div className = {s.postsBlock}>
                 <h3>My posts</h3>
@@ -15,8 +22,8 @@ const MyPosts = () => {
                    </div>
                 </div>
                 <div className={s.posts}>
-                    <Post message='Hi, how are you?'/>
-                    <Post message="It's my first post"/>
+                    <Post message={postData[0].message} likesCount = {postData[0].likesCount}/>
+                    <Post message={postData[1].message} likesCount = {postData[1].likesCount}/>
                 </div>
             </div>
     );
