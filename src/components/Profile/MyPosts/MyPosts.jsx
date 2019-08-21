9f28cacfdd38020eada77626636b25a68f31c82a
Narek Karapetyan <narek.karapetyan.v@gmail.com>
2019-08-21 11:36:44 +0400

    page-version 1.26 (map)

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,27 +5,30 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
 
     let postData = [
-        {id: 1, message: 'Hi, how are you?', likesCount: 12},
-        {id: 2, message: 'It\'s my first post', likesCount: 11}
+        { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+        { id: 2, message: 'It\'s my first post', likesCount: 11 },
+        { id: 3, message: 'Blabla', likesCount: 7 },
+        { id: 4, message: 'Dada', likesCount: 1 }
     ];
 
+    let postsElements = postData
+        .map(p => <Post message={p.message} likesCount={p.likesCount} />);
 
     return (
-            <div className = {s.postsBlock}>
-                <h3>My posts</h3>
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div>
                 <div>
-                    <div>
-                   <textarea></textarea>
-                   </div>
-                   <div>
-                   <button>Add post</button>
-                   </div>
+                    <textarea></textarea>
                 </div>
-                <div className={s.posts}>
-                    <Post message={postData[0].message} likesCount = {postData[0].likesCount}/>
-                    <Post message={postData[1].message} likesCount = {postData[1].likesCount}/>
+                <div>
+                    <button>Add post</button>
                 </div>
             </div>
+            <div className={s.posts}>
+                { postsElements }
+            </div>
+        </div>
     );
 }
 
